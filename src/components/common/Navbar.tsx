6a064360c2ef18c1d240c1e5fcb39bc966ff2865
Veer-Tech-Projects <veertechprojects@gmail.com>
2025-12-20 12:35:33 +0530

    logo updated

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -62,7 +62,7 @@ export default function Navbar() {
               <Link href="/" className="flex items-center gap-3">
                 <div className="relative h-12 w-12">
                   <Image
-                    src="/images/logo/new-logo.png"
+                    src="/images/hero/new-logo.png"
                     alt="JVM Polytechnic Logo"
                     fill
                     className="object-contain"
